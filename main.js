b7670649ced930707613c5882db0bcbb777e719c
eamedema <eamedema@gmail.com>
2019-03-21 21:09:21 -0400

    added jQuery functionality

---
+++ main.js
@@ -0,0 +1,44 @@
+$(()=>{
+
+  let total;
+  let type;
+  let date;
+  let name;
+  let amount;
+
+  $('.enterTotal').on('click', totalMoney);
+
+  function totalMoney(){
+    total = prompt("How much money you got?","Enter a number");
+    $('.total').append(`<h1>$${total}</h1>`);
+    $('.total').removeClass('enterTotal');
+  }
+
+
+  $('.saveButton').on('click', submitEntry);
+
+  function submitEntry(){
+    type = $('.category').val()
+    date = $('.dateOfEntry').text()
+    name = $('.nameInput').text()
+    amount = $('.amountSpent').val()
+
+    $('#listItems').append(`<div class="listItem ${type}">
+    <p class="entry date">${date}</p>
+    <p class="entry name">${name}</p>
+    <p class="entry amount">${amount}<p>
+    </div>`);
+
+    total -= amount;
+    $('.total').removeClass(`total`);
+    $('.total').append(`<h1>$${total}</h1>`);
+  };
+
+
+
+
+
+
+
+
+});
