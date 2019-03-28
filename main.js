37e7ab53ca244ff949b4c0e046e100092f5ef02a
unknown <megan.gallimore@gmail.com>
2019-03-27 21:27:33 -0400

    notes

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 $(()=>{
 
+  $('#newEntry').hide()
+
   let total;
   let type;
   let date;
@@ -18,15 +20,20 @@ $(()=>{
   $('.saveButton').on('click', submitEntry);
 
   function submitEntry(){
-    type = $('.category').val()
+    type = $('#spending-select').val()
     date = $('.dateOfEntry').val()
     name = $('.nameInput').val()
     amount = $('.amountSpent').val()
 
+    console.log(type);
+    console.log(date);
+    console.log(name);
+    console.log(amount);
+
     $('#listItems').append(`<div class="listItem ${type}">
-    <p class="entry date">${date}</p>
-    <p class="entry name">${name}</p>
-    <p class="entry amount">${amount}</p>
+    <p class="entry date" id="left">${date}</p>
+    <p class="entry name" id="left">${name}</p>
+    <p class="entry price">$${amount}</p>
     </div>`);
 
     total -= amount;
@@ -38,16 +45,71 @@ $(()=>{
 
 
 
-  let $btns = $('.btn').click(function() {
-    if (this.id == 'all') {
-      $('#parent > div').fadeIn(450);
-    } else {
-      var $el = $('.' + this.id).fadeIn(450);
-      $('#parent > div').not($el).hide();
-    }
-    $btns.removeClass('active');
-    $(this).addClass('active');
-  })  
+  // let $btns = $('.btn').click(function() {
+  //   if (this.id == 'all') {
+  //     $('#parent > div').fadeIn(450);
+  //   } else {
+  //     var $el = $('.' + this.id).fadeIn(450);
+  //     $('#parent > div').not($el).hide();
+  //   }
+  //   $btns.removeClass('active');
+  //   $(this).addClass('active');
+  // })
+
+  $('#all').on('click', ()=>{
+    $('.food').show()
+    $('.bills').show()
+    $('.clothing').show()
+    $('.entertain').show()
+  });
+
+  $('#food').on('click', ()=>{
+    $('.food').show()
+    $('.bills').show()
+    $('.clothing').show()
+    $('.entertain').show()
+    $('.bills').hide()
+    $('.clothing').hide()
+    $('.entertain').hide()
+  });
+
+  $('#bills').on('click', ()=>{
+    $('.food').show()
+    $('.bills').show()
+    $('.clothing').show()
+    $('.entertain').show()
+    $('.food').hide()
+    $('.clothing').hide()
+    $('.entertain').hide()
+  });
+
+  $('#clothing').on('click', ()=>{
+    $('.food').show()
+    $('.bills').show()
+    $('.clothing').show()
+    $('.entertain').show()
+    $('.bills').hide()
+    $('.food').hide()
+    $('.entertain').hide()
+  });
+
+  $('#entertain').on('click', ()=>{
+    $('.food').show()
+    $('.bills').show()
+    $('.clothing').show()
+    $('.entertain').show()
+    $('.bills').hide()
+    $('.clothing').hide()
+    $('.food').hide()
+  });
+
+  $('#new').on('click', ()=>{
+    $('#newEntry').show()
+  });
+
+  $('#buttonId').on('click', ()=>{
+    $('#newEntry').hide()
+  });
 
 
 });
