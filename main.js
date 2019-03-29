300b930bb18094cae62ae82ef5575c66ddc7d687
eamedema <eamedema@gmail.com>
2019-03-28 23:27:04 -0400

    merging fixed everything.

--- main.js
+++ main.js
@@ -2,126 +2,153 @@ $(()=>{
 
   $('#newEntry').hide()
 
-  let total;
-  let type;
-  let date;
-  let name;
-  let amount;
+ //initializing variables
+ let total;
+ let type;
+ let date;
+ let name;
+ let amount;
+ let amountSpent = 0;
 
-  $('.enterTotal').on('click', totalMoney);
+ //for tracking how much spent on each category
+ let foodTotal = 0;
+ let billsTotal = 0;
+ let clothingTotal = 0;
+ let entertainmentTotal = 0;
 
-  function totalMoney(){
-    total = prompt("How much money you got?","Enter a number");
-    $('.total').append(`<h1 class="totalRemaining">$${total}</h1>`);
-    $('.enterTotal').hide();
-  }
+ // filling the totals
+ $('.amountSpent').append(`<h2 class="totalsTest">TOTAL SPENT = $${amountSpent}</h2>`);
+ $('.foodTotal').append(`<h2 class="totalsTest">FOOD = $${foodTotal}</h2>`);
+ $('.billsTotal').append(`<h2 class="totalsTest">BILLS = $${billsTotal}</h2>`);
+ $('.entertainmentTotal').append(`<h2 class="totalsTest">ENTERTAINMENT = $${entertainmentTotal}</h2>`);
+ $('.clothingTotal').append(`<h2 class="totalsTest">CLOTHING = $${clothingTotal}</h2>`);
 
 
-  $('.saveButton').on('click', submitEntry);
 
-  function submitEntry(){
-    type = $('#spending-select').val()
-    date = $('.dateOfEntry').val()
-    name = $('.nameInput').val()
-    amount = $('.amountSpent').val()
+ $('.enterTotal').on('click', totalMoney);
 
-    // console.log(type);
-    // console.log(date);
-    // console.log(name);
-    // console.log(amount);
+ function totalMoney(){
+   total = prompt("How much money you got?","Enter a number");
+   $('.total').append(`<h1 class="totalRemaining">$${total}</h1>`);
+   $('.enterTotal').hide();
+ }
 
-    $('#listItems').append(`<div class="listItem ${type}">
-    <p class="entry date" id="left">${date}</p>
-    <p class="entry name" id="left">${name}</p>
-    <p class="entry price">$ <em class="numberTotal">${amount}</em></p>
-    </div>`);
 
-    total -= amount;
-    //$('.total').removeClass(`total`);
-    $('.total').html(`<h1 class="totalRemaining">$${total}</h1>`);
+ $('.saveButton').on('click', submitEntry);
 
-    if (total > 0) {
-      $('.total').html(`<h1 class="totalRemaining">$${total}</h1>`);
-    } else if (total <= 0) {
-      alert("Your out of money");
-    }
-  };
+ function submitEntry(){
+   type = $('#spending-select').val()
+   date = $('.dateOfEntry').val()
+   name = $('.nameInput').val()
+   amount = parseInt($('#asdf').val())
 
-  //let numberTotal = $('.numberTotal').html();
-  //$('.total') =-
-  // let totals = $( ":nth-child(3)" ).nextAll( ".price" ).val();
-  // console.log(totals);
+   // console.log(type);
+   // console.log(date);
+   // console.log(name);
+   // console.log(amount);
 
-  // let numberTotal = $('.numberTotal').val()
-  // console.log(numberTotal)
+   if (total > 0) {
+     $('.total').html(`<h1 class="totalRemaining">$${total}</h1>`);
+   } else if (total <= 0) {
+     alert("Your out of money");
+   }
 
-  document.querySelector("#today").valueAsDate = new Date();
 
-  // let $btns = $('.btn').click(function() {
-  //   if (this.id == 'all') {
-  //     $('#parent > div').fadeIn(450);
-  //   } else {
-  //     var $el = $('.' + this.id).fadeIn(450);
-  //     $('#parent > div').not($el).hide();
-  //   }
-  //   $btns.removeClass('active');
-  //   $(this).addClass('active');
-  // })
 
-  $('#all').on('click', ()=>{
-    $('.food').show()
-    $('.bills').show()
-    $('.clothing').show()
-    $('.entertain').show()
-  });
 
-  $('#food').on('click', ()=>{
-    $('.food').show()
-    $('.bills').show()
-    $('.clothing').show()
-    $('.entertain').show()
-    $('.bills').hide()
-    $('.clothing').hide()
-    $('.entertain').hide()
-  });
+   if (total <= 0){
+     alert('You have to enter your total budget');
+   }
+   else if (total > 0){
+     type = $('#spending-select').val();
+     date = $('.dateOfEntry').val();
+     name = $('.nameInput').val();
+     amount = parseInt($('#asdf').val());
 
-  $('#bills').on('click', ()=>{
-    $('.food').show()
-    $('.bills').show()
-    $('.clothing').show()
-    $('.entertain').show()
-    $('.food').hide()
-    $('.clothing').hide()
-    $('.entertain').hide()
-  });
+     //logic to not submit form if there are missing blanks
+     if (type === "") {
+       alert('You have to pick a category');
+       return;
+     };
+     if (date === "") {
+       alert('You need to enter the date');
+       return;
+     };
+     if (name === "") {
+       alert('You need to name your transaction');
+       return;
+     };
+     if (amount === "") {
+       alert('You need to enter a transaction amount');
+       return;
+     };
 
-  $('#clothing').on('click', ()=>{
-    $('.food').show()
-    $('.bills').show()
-    $('.clothing').show()
-    $('.entertain').show()
-    $('.bills').hide()
-    $('.food').hide()
-    $('.entertain').hide()
-  });
+     // console.log(type);
+     // console.log(date);
+     // console.log(name);
+     // console.log(amount);
 
-  $('#entertain').on('click', ()=>{
-    $('.food').show()
-    $('.bills').show()
-    $('.clothing').show()
-    $('.entertain').show()
-    $('.bills').hide()
-    $('.clothing').hide()
-    $('.food').hide()
-  });
 
-  $('#new').on('click', ()=>{
-    $('#newEntry').show()
-  });
+     $('#listItems').append(`<div class="listItem ${type}">
+     <p class="entry date" id="left">${date}</p>
+     <p class="entry name" id="left">${name}</p>
+     <p class="entry price">$${amount}</p>
+     </div>`);
 
-  $('#buttonId').on('click', ()=>{
-    $('#newEntry').hide()
-  });
+     total -= amount;
+     console.log(total);
+     //$('.total').removeClass(`total`);
+     $('.total').html(`<h1 class="totalRemaining">$${total}</h1>`)
+
+     if (total > 0) {
+       $('.total').html(`<h1 class="totalRemaining">$${total}</h1>`)
+     }
+     else if (total <= 0) {
+       total += parseInt(amount);
+       $('.total').html(`<h1 class="totalRemaining">$${total}</h1>`)
+       alert("You do not have enough money for this transaction");
+     };
+
+
+
+
+     //totals editing
+     amountSpent += parseInt(amount);
+     $('.amountSpent').html(`<h2 class="totalsTest">TOTAL SPENT = $${amountSpent}</h2>`)
+
+     if (type ==="food") {
+       foodTotal += parseInt(amount);
+       $('.foodTotal').html(`<h2 class="totalsTest">FOOD = $${foodTotal}</h2>`)
+     }
+     if (type === "bills") {
+       billsTotal += parseInt(amount);
+       $('.billsTotal').html(`<h2 class="totalsTest">BILLS = $${billsTotal}</h2>`)
+     }
+     if (type === "entertainment") {
+       entertainmentTotal += parseInt(amount);
+       $('.entertainmentTotal').html(`<h2 class="totalsTest">ENTERTAINMENT = $${entertainmentTotal}</h2>`)
+     }
+     if (type === "clothing") {
+       clothingTotal += parseInt(amount);
+       $('.clothingTotal').html(`<h2 class="totalsTest">CLOTHING = $${clothingTotal}</h2>`)
+     }
+
+     $('#newEntry').hide()
+   }
+ };
+
+ document.querySelector("#today").valueAsDate = new Date();
+
+ $('#new').on('click',()=>{
+   $('#newEntry').show()
+ });
+
+ $('#buttonId').on('click', ()=>{
+   $('#newEntry').hide()
+ });
+ // $('#buttonId').on('click', ()=>{
+ //   $('#newEntry').hide()
+ // });
 
 
 });
